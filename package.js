a3c40e4b7cc1adcd1737e790ed7937e91e03ff4b
jcahal <jacahal@gmail.com>
2014-04-16 15:34:25 +0000

    Trying to get it to work

--- package.js
+++ package.js
@@ -5,22 +5,20 @@ Package.describe({
 Package.on_use(function (api) {
   api.use('jquery');
 
-  var path = Npm.require('path');
-  var asset_path = path.join('lib');
-  api.add_files(path.join(asset_path, 'css', 'bootstrap.css'), 'client');
-  api.add_files(path.join(asset_path, 'css', 'bootstrap-theme.css'), 'client');
-  api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
+  api.add_files('/Monitor/packages/bootstrap3-fontawesome4/lib/css/bootstrap.css', 'client');
+//  api.add_files(path.join(asset_path, 'css', 'bootstrap-theme.css'), 'client');
+//  api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
 
   // fonts
-  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.eot'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.ttf'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.svg'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.woff'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'FontAwesome.otf'), 'client');
+//  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.eot'), 'client');
+//  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.ttf'), 'client');
+//  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.svg'), 'client');
+//  api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.woff'), 'client');
+//  api.add_files(path.join(asset_path, 'fonts', 'FontAwesome.otf'), 'client');
 
   // XXX this makes the paths to the icon sets absolute. it needs
   // to be included _after_ the standard bootstrap css so
   // that its styles take precedence.
-  api.add_files(path.join(asset_path, 'css', 'font-awesome.css'), 'client');
-  api.add_files(path.join('override.css'), 'client');
+//  api.add_files(path.join(asset_path, 'css', 'font-awesome.css'), 'client');
+//  api.add_files(path.join('override.css'), 'client');
 });
